59a81c9c4e9e451f7e71ace33fff81421e37a290
Sefa AŞIK <sefa.a1707@gmail.com>
2024-02-13 20:58:38 +0300

    Add project files.

---
+++ MvcKutuphaneWebProject/Controllers/AdminKitapController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcKutuphaneWebProject.Controllers
+{
+    public class AdminKitapController : Controller
+    {
+        // GET: AdminKitap
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
